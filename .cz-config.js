45e0de0bdfe390e8a71d31ebc4a691f8a197e139
Antonio <amarquez1980@gmail.com>
2021-04-05 09:17:27 +0200

    feat: installed and configured Prettier

--- .cz-config.js
+++ .cz-config.js
@@ -1,37 +1,38 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
-    { value: 'docs', name: 'docs:     Documentation only changes' },
+    { value: "feat", name: "feat:     A new feature" },
+    { value: "fix", name: "fix:      A bug fix" },
+    { value: "docs", name: "docs:     Documentation only changes" },
     {
-      value: 'style',
+      value: "style",
       name:
-        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+        "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)",
     },
     {
-      value: 'refactor',
-      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
-    },
-    {
-      value: 'perf',
-      name: 'perf:     A code change that improves performance',
-    },
-    { value: 'test', name: 'test:     Adding missing tests' },
-    {
-      value: 'chore',
+      value: "refactor",
       name:
-        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+        "refactor: A code change that neither fixes a bug nor adds a feature",
     },
-    { value: 'revert', name: 'revert:   Revert to a commit' },
-    { value: 'WIP', name: 'WIP:      Work in progress' },
+    {
+      value: "perf",
+      name: "perf:     A code change that improves performance",
+    },
+    { value: "test", name: "test:     Adding missing tests" },
+    {
+      value: "chore",
+      name:
+        "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation",
+    },
+    { value: "revert", name: "revert:   Revert to a commit" },
+    { value: "WIP", name: "WIP:      Work in progress" },
   ],
 
-  scopes: [{ name: 'TO BE CHANGED' }],
+  scopes: [{ name: "TO BE CHANGED" }],
 
   allowTicketNumber: false,
   isTicketNumberRequired: false,
-  ticketNumberPrefix: 'TICKET-',
-  ticketNumberRegExp: '\\d{1,5}',
+  ticketNumberPrefix: "TICKET-",
+  ticketNumberRegExp: "\\d{1,5}",
 
   // it needs to match the value for field type. Eg.: 'fix'
   /*
@@ -47,20 +48,22 @@ module.exports = {
   // override the messages, defaults are as follows
   messages: {
     type: "Select the type of change that you're committing:",
-    scope: '\nDenote the SCOPE of this change (optional):',
+    scope: "\nDenote the SCOPE of this change (optional):",
     // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+    customScope: "Denote the SCOPE of this change:",
+    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+    body:
+      'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: "List any BREAKING CHANGES (optional):\n",
+    footer:
+      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
+    confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix'],
+  allowBreakingChanges: ["feat", "fix"],
   // skip any questions you want
-  skipQuestions: ['body'],
+  skipQuestions: ["body"],
 
   // limit subject length
   subjectLimit: 100,
